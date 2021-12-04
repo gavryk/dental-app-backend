1e5624e46ae8654690ca56d99b5663a3a54c1445
gvozdyk <gvozdyk23@gmail.com>
2021-12-05 00:43:45 +0200

    it's new for me

--- models/Patient.js
+++ models/Patient.js
@@ -1,2 +1,17 @@
-const { Shema } = require('mongoose');
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
+const PatientSchema = new Schema(
+  {
+    id: String,
+    fullname: String,
+    phone: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Patient = mongoose.model("Patient", PatientSchema);
+
+module.exports = Patient;
